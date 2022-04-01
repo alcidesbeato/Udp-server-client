15338faa5908579d9f2854708b55a7c43fb9497e
alcidesbeato <71664777+alcidesbeato@users.noreply.github.com>
2022-04-01 09:54:37 -0300

    update client and server

--- socket_client.py
+++ socket_client.py
@@ -1,23 +1,36 @@
+from datetime import datetime
 import socket
 import sys
 import json
 
+sock = socket.socket()
+sock.bind(('', 0))
+porta_origem = sock.getsockname()
 
+ip_destino = input("Digite o IP destino: ")
+porta_destino = int(input("Digite a porta de destino: "))
+msg = input("Digite a mensagem a ser enviada: ")
+
+hostname = socket.gethostname()
+ip_origem = socket.gethostbyname(hostname)
+time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
 jsonResult = {
-        "first":"You're",
-        "second":"Awsome"}
+        "IP_origem" : ip_origem,
+        "IP_destino" : ip_destino,
+        "Porta_Origem":porta_origem,
+        "Porta_Destino" : porta_destino,
+        "Time_Stamp": time,
+        "Mensagem" : msg}
 jsonResult = json.dumps(jsonResult)
-print (jsonResult)
+
+
 try:
     sock = socket.socket()
 except socket.error as err:
     print ('Socket error because of %s') %(err)
 
-port = 1500
-address = "192.168.0.4"
-
 try:
-    sock.connect((address, port))
+    sock.connect((ip_destino, porta_destino))
     sock.send(bytearray(jsonResult,'utf-8'))
 except socket.gaierror:
 
--- socket_server.py
+++ socket_server.py
@@ -1,4 +1,7 @@
 import socket
+from datetime import datetime
+import sys
+import json
 
 sock = socket.socket()
 print ('Socket created ...')
@@ -12,10 +15,44 @@ print ('socket is listening')
 while True:
     c, addr = sock.accept()
     print ('got connection from ', addr)
-
+   
     jsonReceived = c.recv(1024)
     print ("Json received -->", jsonReceived)
-
-    c.close()
-
-
+    
+    if jsonReceived:
+     print("msg recebida")
+     print("dc")
+     x = json.loads(jsonReceived)
+     ip_origem = x["IP_origem"]
+     ip_destino = x["IP_destino"]
+     porta_origem = x["Porta_Origem"]
+     porta_destino = x["Porta_Destino"]
+     time_client = x["Time_Stamp"]
+     time = datetime.now().strftime("%d/%m/%Y, %H:%M:%S")
+     jsonResult = {
+        "IP_origem" : ip_origem,
+        "IP_destino" : ip_destino,
+        "Porta_Origem":porta_origem,
+	    "Porta_Destino" : porta_destino,
+	    "Time_Stamp": time_client,
+	    "Time_destino": time,
+	    "ACK": True}
+     jsonResult = json.dumps(jsonResult)
+     try:
+      sock = socket.socket()
+     except socket.error as err:
+      print ('Socket error because of %s') %(err)
+     try:
+      print("ip de destino", ip_destino)
+      print("porta destino", porta_destino)
+      print("addr", addr)
+      sock.connect((ip_destino, porta_destino))
+      sock.send(bytearray(jsonResult,'utf-8'))
+     except socket.gaierror:
+      print ('There an error resolving the host')
+      sys.exit() 	
+     print (jsonResult, 'was sent!')
+     sock.close()   
+    else:
+     print("msg nao recebida")	
+c.close()
